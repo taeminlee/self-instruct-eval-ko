256831fe4ee68d146e0d5cd4b2cc87680c544af3
Taemin Lee <persuade@gmail.com>
2023-04-13 19:34:55 +0900

    update: gpt-4 request time 60 -> 600

--- translate.py
+++ translate.py
@@ -134,7 +134,7 @@ class ChatGPT(Translate):
 
     def __call__(self, original_text: str, target_lang: str = 'Korean') -> str:
         """
-        입력된 original_text를 GPT-3 모델을 이용하여 target_lang으로 번역하는 함수입니다. 
+        입력된 original_text를 ChatGPT-3.5 모델을 이용하여 target_lang으로 번역하는 함수입니다. 
 
         Args:
           original_text (str): 번역할 영어 텍스트
@@ -155,7 +155,7 @@ class ChatGPT(Translate):
 class GPT4(Translate):
     def __init__(self, model_name='gpt-4'):
         # ChatOpenAI 객체를 생성합니다.
-        chat = ChatOpenAI(model_name=model_name)
+        chat = ChatOpenAI(model_name=model_name, request_timeout=600)
         # 사용자에게 보낼 시스템 메시지와 인간의 응답 메시지 템플릿을 정의합니다.
         template = "You are a helpful assistant that translates English to {target_lang}. Please paraphrase as much as possible when translating. Do not add expressions that are not in the source sentences. Do not add pronunciations for the target language. Please provide the {target_lang} translation for these sentences:"
         system_message_prompt = SystemMessagePromptTemplate.from_template(
@@ -171,7 +171,7 @@ class GPT4(Translate):
 
     def __call__(self, original_text: str, target_lang: str = 'Korean') -> str:
         """
-        입력된 original_text를 GPT-3 모델을 이용하여 target_lang으로 번역하는 함수입니다. 
+        입력된 original_text를 GPT-4 모델을 이용하여 target_lang으로 번역하는 함수입니다. 
 
         Args:
           original_text (str): 번역할 영어 텍스트
